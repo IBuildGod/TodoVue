199885e33d27cd814793a53d315b4ae725f96134
DESKTOP-MJKU67N\johnr <johnrencveluz@gmail.com>
2021-09-19 18:31:48 +0800

    working add and remove task

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,74 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="Todo">
+  <form v-on:submit.prevent="addNewTask">
+    <label for="new-Task">Add a task </label>
+    <input
+      v-model="newTaskText"
+      id="new-task"
+      placeholder="My new task"
+    />
+    <button>Add</button>
+  </form>
+
+  <ul>
+    <li v-for="(task, index) in tasks"
+      :key="task.id"
+      :title="task.title">
+        {{ task.title }}
+      <button @click="removeTask(index, tasks)">Done</button>
+    </li>
+  </ul>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import Tasks from './Components/Task'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+   data() {
+    return {
+      newTaskText: '',
+      tasks: [
+        {
+          id: 1,
+          title: 'task 1'
+        },
+        {
+          id: 2,
+          title: 'task 2'
+        },
+        {
+          id: 3,
+          title: 'task 3'
+        }
+      ],
+    }
+  },
+  methods: {
+    addNewTask() {
+      this.tasks.push({
+        id: this.id++,
+        title: this.newTaskText
+      })
+    },
+    removeTask(int, object){
+      object.splice(int, 1)
+    }
+  },
+  props: ['title'],
+  emits: ['remove'],
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.demo {
+  font-family: sans-serif;
+  border: 1px solid #eee;
+  border-radius: 2px;
+  padding: 20px 30px;
+  margin-top: 1em;
+  margin-bottom: 40px;
+  user-select: none;
+  overflow-x: auto;
 }
 </style>
