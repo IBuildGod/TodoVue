d35e149a008bedc91ebc6e00c3f1fe3d946bdad7
DESKTOP-MJKU67N\johnr <johnrencveluz@gmail.com>
2021-09-22 21:29:24 +0800

    additional features

--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,21 @@
     <li v-for="(task, index) in tasks"
       :key="task.id"
       :title="task.title"
-      class="text-white pl-2 list-disc my-1 break-words w-full">
+      :priority="task.priority"
+      :color="task.color"
+      class="text-white list-disc break-words pl-2 my-1 w-full"
+      :style="{background: task.color}"
+      >
         {{ task.title }}
-      <button @click="removeTask(index, tasks)" class="float-right mr-3 hover:bg-gray-300 rounded px-2 place-items-center hover:text-black">x</button>
+        <div class="relative inline-block float-right">
+            <select @change="onChange($event, index)" class=" text-white border-2 border-gray-300 rounded z-1 bg-gray-500 mr-2">
+              <option disabled selected value>Set Priority</option>
+              <option v-for="item in importance" :key="item.id" :value="item.rating">
+                {{ item.rating }}
+              </option>
+            </select>
+          </div>
+      <button @click="removeTask(index, tasks, task.id, task.title, task.priority, task.color)" class="float-right hover:bg-gray-300 mr-3 rounded px-2 place-items-center hover:text-black">x</button>
     </li>
   </ul>
 </div>
@@ -29,22 +41,62 @@
 export default {
    data() {
     return {
+      newColor: '',
       newTaskText: '',
       tasks: [
         {
           id: 1,
-          title: 'task 1'
+          title: 'task 1',
+          priority: 0,
+          color: '#424242',
         },
         {
           id: 2,
-          title: 'task 2'
+          title: 'task 2',
+          priority: 0,
+          color: '#424242',
         },
         {
           id: 3,
-          title: 'task 3'
+          title: 'task 3',
+          priority: 0,
+          color: '#424242',
         }
       ],
       newtaskId: 4,
+      deletedtasks: [{}],
+      importance: [
+        {
+          id: 1,
+          rating: 1,
+          backgroundColor: 'red',
+        },
+        {
+          id: 2,
+          rating: 2,
+          backgroundColor: 'orange',
+        },
+        {
+          id: 3,
+          rating: 3,
+          backgroundColor: '#8B8000',
+        },
+        {
+          id: 4,
+          rating: 4,
+          backgroundColor: 'green',
+        },
+        {
+          id: 5,
+          rating: 5,
+          backgroundColor: 'blue',
+        },
+      ],
+      dropdownItems: [{
+        text:'Set priority',
+        index: 1
+      }, 
+      ],
     }
   },
   methods: {
@@ -55,13 +107,24 @@ export default {
         id: this.newtaskId++,
         title: this.newTaskText,
         newTaskText: "",
+        color: '#424242',
       })
     },
-    removeTask(taskIndex, listName){
+    removeTask(taskIndex, listName, id, title, priority, color){
       listName.splice(taskIndex, 1)
-    }
+      this.deletedtasks.push({
+        title: title,
+        id: id,
+        priority: priority,
+        color: color,
+      }),
+      console.log(this.deletedtasks)
+    },
+    onChange(event, index) {
+      this.tasks[index].color = this.importance[event.target.value - 1].backgroundColor
+      this.tasks[index].priority = event.target.value
+      console.log(this.importance[event.target.value - 1].backgroundColor)
+    },
   },
-  props: ['title'],
-  emits: ['remove'],
 }
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    maxWidth: {
+      '1/4': '25%',
+      '1/2': '50%',
+      '3/4': '75%',
+     },
     extend: {},
     colors: {
       transparent: 'transparent',
@@ -17,7 +22,9 @@ module.exports = {
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      display: ["group-hover"],
+    },
   },
   plugins: [],
 }
